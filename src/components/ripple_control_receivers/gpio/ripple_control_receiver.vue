941b52142c4d6c301bf4fd21d157a69aae244f69
Lutz Bender <github@lutz-bender.de>
2024-01-29 12:56:32 +0100

    add gpio configuration

--- src/components/ripple_control_receivers/gpio/ripple_control_receiver.vue
+++ src/components/ripple_control_receivers/gpio/ripple_control_receiver.vue
@@ -4,6 +4,53 @@
 			Wenn mindestens ein Kontakt geschlossen ist, wird die Ladung
 			gesperrt. Wenn beide Kontakte offen sind, darf geladen werden.
 		</openwb-base-alert>
+		<openwb-base-heading>
+			Konfiguration der Kontakte
+		</openwb-base-heading>
+		<openwb-base-select-input
+			class="mb-2"
+			title="RSE 1 offen, 2 offen"
+			:options="[
+				{value: 100, text: '- keine Aktion -'},
+				{value: 0, text: 'Ladung beenden'},
+			]"
+			:model-value="100"
+			disabled
+			readonly
+		/>
+		<openwb-base-select-input
+			class="mb-2"
+			title="RSE 1 geschlossen, 2 offen"
+			:options="[
+				{value: 100, text: '- keine Aktion -'},
+				{value: 0, text: 'Ladung beenden'},
+			]"
+			:model-value="0"
+			disabled
+			readonly
+		/>
+		<openwb-base-select-input
+			class="mb-2"
+			title="RSE 1 offen, 2 geschlossen"
+			:options="[
+				{value: 100, text: '- keine Aktion -'},
+				{value: 0, text: 'Ladung beenden'},
+			]"
+			:model-value="0"
+			disabled
+			readonly
+		/>
+		<openwb-base-select-input
+			class="mb-2"
+			title="RSE 1 geschlossen, 2 geschlossen"
+			:options="[
+				{value: 100, text: '- keine Aktion -'},
+				{value: 0, text: 'Ladung beenden'},
+			]"
+			:model-value="0"
+			disabled
+			readonly
+		/>
 	</div>
 </template>
 
