333b883dbdcff5c3a6bb1c08360847a2ee46b0ad
Lutz Bender <github@lutz-bender.de>
2024-02-05 07:27:33 +0100

    add button input test

--- src/components/__tests__/OpenwbBaseButtonInput.spec.js
+++ src/components/__tests__/OpenwbBaseButtonInput.spec.js
@@ -1,18 +1,61 @@
-import { describe, it, expect } from "vitest";
-import { mount } from "@vue/test-utils";
+import { shallowMount } from "@vue/test-utils";
 import OpenwbBaseButtonInput from "../OpenwbBaseButtonInput.vue";
 
-describe("OpenwbBaseButtonInput.vue", () => {
-  // general constants
-  const title = "Button Test";
-  const buttonText = "Test Button";
-  const buttonSubtype = "success";
-  // check title prop
-  it("render title", () => {
-    const wrapper = mount(OpenwbBaseButtonInput, {
-      props: { title, buttonText, buttonSubtype },
+describe("OpenwbBaseButtonInput", () => {
+  it("renders correctly", () => {
+    const wrapper = shallowMount(OpenwbBaseButtonInput, {
+      props: {
+        title: "Test Title",
+        buttonText: "Test Button",
+        disabled: false,
+        subtype: "secondary",
+      },
     });
-    const titleLabel = wrapper.find("label.col-form-label");
-    expect(titleLabel.html()).toContain(title);
+
+    expect(wrapper.exists()).toBe(true);
+    expect(wrapper.find(".col-form-label").text()).toBe("Test Title");
+    expect(wrapper.find(".btn-secondary").text()).toBe("Test Button");
+    expect(wrapper.find(".btn-secondary").attributes("disabled")).toBe("false");
+    expect(wrapper.find(".text-info").exists()).toBe(false);
+    expect(wrapper.find(".alert-info").exists()).toBe(false);
+  });
+
+  it("toggles help correctly", async () => {
+    const wrapper = shallowMount(OpenwbBaseButtonInput, {
+      props: {
+        title: "Test Title",
+        buttonText: "Test Button",
+        disabled: false,
+        subtype: "secondary",
+      },
+      slots: {
+        help: "Test Help",
+      },
+    });
+
+    expect(wrapper.find(".alert-info").exists()).toBe(false);
+
+    await wrapper.find(".col-form-label").trigger("click");
+
+    expect(wrapper.find(".alert-info").exists()).toBe(true);
+
+    await wrapper.find(".col-form-label").trigger("click");
+
+    expect(wrapper.find(".alert-info").exists()).toBe(false);
+  });
+
+  it("emits buttonClicked event correctly", () => {
+    const wrapper = shallowMount(OpenwbBaseButtonInput, {
+      props: {
+        title: "Test Title",
+        buttonText: "Test Button",
+        disabled: false,
+        subtype: "secondary",
+      },
+    });
+
+    wrapper.find(".btn-secondary").trigger("click");
+
+    expect(wrapper.emitted("buttonClicked"));
   });
 });
